d01cdbbc1bbb0b21850002cad8f66c135a152f25
jeffsoncavalcante <jeffson.cavalcante@ftc.edu.br>
2021-09-27 21:55:16 -0300

    Fix: Ajuste no codigo de teste

--- teste/teste.js
+++ teste/teste.js
@@ -5,8 +5,20 @@ document.addEventListener('DOMContentLoaded', function(){
            
            var vetor = ['um','dois']
            window.localStorage.setItem("key", vetor)
-           console.log(document.head.style)
-           document.body.style.background = "#ccc";
+           let el = document.querySelectorAll('h1')
+           
+         for (i=0; i<el.length; i++){
+            var styles = window.getComputedStyle(el[i]);
+             
+             var css = Object.keys(styles).reduce(
+               (obj, prop) => (prop.match(/\D/) && (obj[prop] = styles[prop]), obj), {}
+             );
+           console.log(css.color)
+            if(css.color == "rgb(255, 0, 0)" ){
+            el[i].style.cssText = 'color: blue;'
+            }
+         }
+         
         })
     }
    
