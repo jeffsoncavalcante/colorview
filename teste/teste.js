bded9dc091093d528717fcaa1ce4db6cffa788af
jeffsoncavalcante <jeffson.cavalcante@ftc.edu.br>
2021-09-30 20:54:21 -0300

    Fix: Script

--- teste/teste.js
+++ teste/teste.js
@@ -1,25 +1,13 @@
+
 document.addEventListener('DOMContentLoaded', function(){
     var enviar = document.getElementById('enviar');
     if(enviar){
         enviar.addEventListener('click',function teste(){
-           
-           var vetor = ['um','dois']
-           window.localStorage.setItem("key", vetor)
-           let el = document.querySelectorAll('h1')
-           
-         for (i=0; i<el.length; i++){
-            var styles = window.getComputedStyle(el[i]);
-             
-             var css = Object.keys(styles).reduce(
-               (obj, prop) => (prop.match(/\D/) && (obj[prop] = styles[prop]), obj), {}
-             );
-           console.log(css.color)
-            if(css.color == "rgb(255, 0, 0)" ){
-            el[i].style.cssText = 'color: blue;'
-            }
-         }
-         
+            console.log("aqui")
+            var vetor = true
+            window.localStorage.setItem("key", vetor)
         })
     }
-   
-})
+
+        
+    })
